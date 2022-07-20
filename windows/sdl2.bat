f024115f2e20231185b6a3500e95fc493a14379a
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-07-21 00:59:58 +0300

    Always use v140 toolset (#8)

--- windows/sdl2.bat
+++ windows/sdl2.bat
@@ -9,7 +9,12 @@ set VCPKG_DIR=%~1
 set PLATFORM=%~2
 set OUTPUT_DIR=%~3
 
-set TRIPLET=%PLATFORM%-windows
+set TRIPLET=%PLATFORM%-windows-v140
+
+echo Adding triplets...
+
+xcopy /Y /Q "%~dp0\vcpkg_triplets\*.cmake" "%VCPKG_DIR%\triplets\community\" || ^
+exit /B 1
 
 echo Building sdl2 and dependencies...
 
