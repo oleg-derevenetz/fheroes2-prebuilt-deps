98147c8826eb14b4d15ddaf44948601932202138
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-07-20 23:55:23 +0300

    Add hash verification (#7)

--- windows/sdl1.bat
+++ windows/sdl1.bat
@@ -8,15 +8,15 @@ if "%~1"=="" (
 set OUTPUT_DIR=%~1
 
 set SDL_FILE=SDL-devel-1.2.15-VC.zip
-set SDL_MIXER_FILE=SDL_mixer-devel-1.2.12-VC.zip
-
+set SDL_FILE_SHA256=7443EE5CD1BD2A0F9FA8C01ACBEB4C779D0B03F9CE8A2AA47B0637D2A5461E50
 set SDL_URL=https://www.libsdl.org/release/%SDL_FILE%
+
+set SDL_MIXER_FILE=SDL_mixer-devel-1.2.12-VC.zip
+set SDL_MIXER_FILE_SHA256=25084EB2B3C49FEA38D01E24B172109BAE07C0AC82B1765D5C179026A164387B
 set SDL_MIXER_URL=https://www.libsdl.org/projects/SDL_mixer/release/%SDL_MIXER_FILE%
 
-echo Downloading %SDL_URL%                                                                                         && ^
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%SDL_URL%',       '%TEMP%\%SDL_FILE%')"       && ^
-echo Downloading %SDL_MIXER_URL%                                                                                   && ^
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%SDL_MIXER_URL%', '%TEMP%\%SDL_MIXER_FILE%')" || ^
+call :download_archive "%SDL_URL%"       "%TEMP%\%SDL_FILE%"       "%SDL_FILE_SHA256%"       && ^
+call :download_archive "%SDL_MIXER_URL%" "%TEMP%\%SDL_MIXER_FILE%" "%SDL_MIXER_FILE_SHA256%" || ^
 exit /B 1
 
 if not exist "%OUTPUT_DIR%\include"        ( mkdir "%OUTPUT_DIR%\include"        || exit /B 1 )
@@ -37,14 +37,34 @@ exit /B 1
 
 exit /B
 
+:download_archive
+
+echo Downloading %~1
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%~1', '%~2')" || exit /B 1
+
+set HASH_CALCULATED=false
+
+for /F "usebackq delims=" %%H in (`powershell -Command "(Get-FileHash -Path '%~2' -Algorithm SHA256).Hash"`) do (
+    set HASH_CALCULATED=true
+
+    if not "%%H" == "%~3" (
+        echo Invalid hash for %~n1%~x1: expected %~3, got %%H
+        exit /B 1
+    )
+)
+
+if not "%HASH_CALCULATED%" == "true" (
+    echo Failed to calculate hash for %~n1%~x1
+    exit /B 1
+)
+
+exit /B
+
 :unpack_archive
 
 echo Unpacking %~n1%~x1
 
-if "%CI%" == "true" (
-    powershell -Command "Expand-Archive -LiteralPath '%~1' -DestinationPath '%~2' -Force" || exit /B 1
-) else (
-    powershell -Command "$shell = New-Object -ComObject 'Shell.Application'; $zip = $shell.NameSpace((Resolve-Path '%~1').Path); foreach ($item in $zip.items()) { $shell.Namespace((Resolve-Path '%~2').Path).CopyHere($item, 0x14) }" || exit /B 1
-)
+powershell -Command "Expand-Archive -LiteralPath '%~1' -DestinationPath '%~2' -Force" || exit /B 1
 
 exit /B
