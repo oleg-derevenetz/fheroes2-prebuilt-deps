f1524b9e60732bb8d80de2f05dc67c783d04b903
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-10-20 01:20:48 +0300

    Build dependency package for Android, rollback SDL to 2.0.22 (#17)

---
+++ android/Application.mk
@@ -0,0 +1,6 @@
+# Generate code for the following ABIs
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+# Minimum supported Android API level
+APP_PLATFORM := android-19
+# Build SDL2_mixer with TiMidity
+SUPPORT_MID_TIMIDITY := true
---
+++ android/README.md
@@ -0,0 +1,1 @@
+This directory contains necessary files used to pre-build the dependencies of the **fheroes2** project for the Android platform.
