f764e0b25592db2089b00cb019e96f38ec02572f
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2024-11-06 00:49:22 +0300

    Update Android & Windows deps (#56)

--- android/Application.mk
+++ android/Application.mk
@@ -1,7 +1,7 @@
 # Generate code for the following ABIs
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 # Minimum supported Android API level
-APP_PLATFORM := android-19
+APP_PLATFORM := android-21
 # Build SDL2_mixer with TiMidity
 SUPPORT_MID_TIMIDITY := true
 # Build SDL2_mixer without WavPack
